32e7e14f6aea279abe6f09ba40541837aed48077
Amirerfan <amirerfan.siamaki@gmail.com>
2023-06-03 14:26:31 +0200

    Home Page Hero

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,13 @@
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="page">
+      <p className="moto text-white md:w-2/5 text-5xl font-light leading-[58px] mb-4">
+        Pion Network: Be a Pioneer, Bridge the Blockchain Islands
+      </p>
+      <p className="description text-white font-light text-[28px] mb-11">
+        [ Description about the NFT, tiers and verification ]
+      </p>
+      <button className="btn btn--large">Start Your Node</button>
     </div>
   );
 };
