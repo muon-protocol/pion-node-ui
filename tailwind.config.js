b2358acf945e5f6134597f600e122afa19c4e544
Amirerfan <amirerfan.siamaki@gmail.com>
2023-06-08 12:46:04 +0200

    Secondary Button

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,12 @@ export default {
     extend: {
       colors: {
         primary: '#5158F6',
-        'dark-soft-primary': '#5158f640',
+        'dark-soft-primary': '#5158F640',
         'xyz-75': '#E9EFF6BF',
         'soft-sky': '#BCC8DE',
         'primary-10-solid': '#ECEDFF',
-        'card-bg-70-purple': '#6c677bb3',
+        'card-bg-70-purple': '#6C677BB3',
+        'titan-white': '#ECEDFF',
         'plan-1': '#F58E8E80',
         'plan-2': '#F3C95D80',
         'plan-3': '#918EF580',
