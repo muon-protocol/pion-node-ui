7901a565751a437ea97a89713db679c774e16f4e
Amirerfan <amirerfan.siamaki@gmail.com>
2023-06-25 15:46:59 +0200

    Get Allowance

--- src/contexts/ALICE/ALICEContext.tsx
+++ src/contexts/ALICE/ALICEContext.tsx
@@ -1,27 +1,35 @@
 import { createContext, ReactNode, useEffect, useState } from 'react';
-import { useBalance } from 'wagmi';
+import { useBalance, useContractRead } from 'wagmi';
 import useUserProfile from '../UserProfile/useUserProfile.ts';
-import { ALICE_ADDRESS } from '../../constants/addresses.ts';
+import { ALICE_ADDRESS, BONALICE_ADDRESS } from '../../constants/addresses.ts';
 import { getCurrentChainId } from '../../constants/chains.ts';
 import { W3bNumber } from '../../types/wagmi.ts';
 import { w3bNumberFromBigint } from '../../utils/web3.ts';
+import ALICE_ABI from '../../abis/ALICE.json';
 
 const ALICEContext = createContext<{
   ALICEBalanceIsFetched: boolean;
   ALICEBalanceIsLoading: boolean;
   ALICEBalance: W3bNumber | null;
+  allowanceIsFetched: boolean;
+  allowanceIsLoading: boolean;
+  allowance: W3bNumber | null;
 }>({
   ALICEBalanceIsFetched: false,
   ALICEBalanceIsLoading: false,
   ALICEBalance: null,
+  allowanceIsFetched: false,
+  allowanceIsLoading: false,
+  allowance: null,
 });
 
 const ALICEProvider = ({ children }: { children: ReactNode }) => {
   const { walletAddress } = useUserProfile();
   const [ALICEBalance, setALICEBalance] = useState<W3bNumber | null>(null);
+  const [allowance, setAllowance] = useState<W3bNumber | null>(null);
 
   const {
-    data,
+    data: ALICEBalanceData,
     isFetched: ALICEBalanceIsFetched,
     isLoading: ALICEBalanceIsLoading,
   } = useBalance({
@@ -32,10 +40,32 @@ const ALICEProvider = ({ children }: { children: ReactNode }) => {
   });
 
   useEffect(() => {
-    if (ALICEBalanceIsFetched && data && data?.value) {
-      setALICEBalance(w3bNumberFromBigint(data.value));
+    if (ALICEBalanceIsFetched && ALICEBalanceData && ALICEBalanceData?.value) {
+      setALICEBalance(w3bNumberFromBigint(ALICEBalanceData.value));
     }
-  }, [ALICEBalanceIsFetched, data]);
+  }, [ALICEBalanceIsFetched, ALICEBalanceData]);
+
+  const {
+    data: allowanceData,
+    isFetched: allowanceIsFetched,
+    isLoading: allowanceIsLoading,
+  } = useContractRead({
+    abi: ALICE_ABI,
+    address: ALICE_ADDRESS[getCurrentChainId()],
+    functionName: 'allowance',
+    args: [walletAddress, BONALICE_ADDRESS[getCurrentChainId()]],
+    chainId: getCurrentChainId(),
+    watch: true,
+  });
+
+  useEffect(() => {
+    console.log('allowanceIsFetched', allowanceIsFetched);
+    if (allowanceIsFetched && allowanceData) {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      setAllowance(w3bNumberFromBigint(allowanceData));
+    }
+  }, [allowanceIsFetched, allowanceData]);
 
   return (
     <ALICEContext.Provider
@@ -43,6 +73,9 @@ const ALICEProvider = ({ children }: { children: ReactNode }) => {
         ALICEBalanceIsFetched,
         ALICEBalanceIsLoading,
         ALICEBalance,
+        allowanceIsFetched,
+        allowanceIsLoading,
+        allowance,
       }}
     >
       {children}
