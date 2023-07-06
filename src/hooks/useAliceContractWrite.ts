6af30f34b23ba6da904dd8a2cb61d4d2a6d7f64f
Amirerfan <amirerfan.siamaki@gmail.com>
2023-07-06 14:22:31 +0200

    Clean Code

--- src/hooks/useAliceContractWrite.ts
+++ src/hooks/useAliceContractWrite.ts
@@ -24,18 +24,8 @@ const useAliceContractWrite = ({
     if (!abi || !address || !functionName || !args || !chainId) return null;
 
     return async (stateMessage: StateMessages) => {
-      console.log('1');
       setIsMetamaskLoading(true);
-      console.log('2');
       try {
-        console.log('3', {
-          abi: abi,
-          address: address,
-          functionName: functionName,
-          args: args,
-          chainId: chainId,
-        });
-
         const { hash } = await writeContract({
           abi: abi,
           address: address,
@@ -43,30 +33,20 @@ const useAliceContractWrite = ({
           args: args,
           chainId: chainId,
         });
-        console.log('4');
         setIsMetamaskLoading(false);
-        console.log('5');
         if (!hash) throw new Error('No hash returned from writeContract');
-        console.log('6');
         setIsTransactionLoading(true);
-        console.log('7');
         setTransactionHash(hash);
-        console.log('8');
         const transaction = waitForTransaction({ hash });
-        console.log('9');
 
         await toast.promise(transaction, {
           loading: stateMessage.pending,
           success: stateMessage.success,
           error: stateMessage.failed,
         });
-        console.log('10');
         setIsTransactionLoading(false);
-        console.log('11');
       } catch (error) {
-        console.log('12');
         setIsMetamaskLoading(false);
-        console.log('13');
         setIsTransactionLoading(false);
         console.log(error);
       }
