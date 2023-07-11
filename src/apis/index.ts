ad61f81b6b831bb2e558f9e69c09d3aac4c2010a
Amirerfan <amirerfan.siamaki@gmail.com>
2023-07-11 18:07:42 +0200

    Add API Proxy

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,11 +1,6 @@
 import axios from 'axios';
 
-const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.NODE_ENV === 'development'
-      ? ''
-      : import.meta.env.VITE_APP_API_URL,
-});
+const axiosInstance = axios.create();
 
 export async function getRewardsAPI(addresses: string[]) {
   const response = await axiosInstance.get(
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/reward-server': 'http://103.75.196.96:80',
+    },
+  },
+});
