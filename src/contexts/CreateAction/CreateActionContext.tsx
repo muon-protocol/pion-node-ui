4ee5c5c29bf15d343bd018b4cbff86fb8281afc4
Amirerfan Siamaki <amirerfan.siamaki@gmail.com>
2023-07-21 16:06:27 +0200

    Fix Deploy Bug

--- src/contexts/CreateAction/CreateActionContext.tsx
+++ src/contexts/CreateAction/CreateActionContext.tsx
@@ -1,7 +1,7 @@
 import { createContext, ReactNode, useEffect, useState } from 'react';
 import useALICE from '../ALICE/useALICE.ts';
 import BONALICE_ABI from '../../abis/BonALICE.json';
-import { ALICE_ABI } from '../../abis/ALICE.ts';
+import ALICE_ABI from '../../abis/ALICE.ts';
 import {
   ALICE_ADDRESS,
   BONALICE_ADDRESS,
