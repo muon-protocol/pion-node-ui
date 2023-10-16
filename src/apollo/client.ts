bb6054e60f2f21a411b701d823d0996f123f08e5
Amirerfan Siamaki <amirerfan.siamaki@gmail.com>
2023-10-16 12:59:41 +0200

    Change Subgraph Address

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const aliceClient = new ApolloClient({
-  uri: 'https://api.thegraph.com/subgraphs/name/rastegarishirazu/muon-nft',
+  uri: 'https://api.thegraph.com/subgraphs/name/rastegarishirazu/muon-nft-mainnet',
   cache: new InMemoryCache(),
   ssrMode: typeof window === 'undefined',
 });
