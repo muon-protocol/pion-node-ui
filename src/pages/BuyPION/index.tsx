7f34080f3d73adf58b22f26a57f43029ef447c49
Amirerfan Siamaki <amirerfan.siamaki@gmail.com>
2023-10-18 11:59:17 +0200

    Buy PION Page

--- src/pages/BuyPION/index.tsx
+++ src/pages/BuyPION/index.tsx
@@ -44,31 +44,88 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="page__bg--home">
-      <div className="page page--home page--centered flex flex-col md:px-10 gap-6">
+    <div className="page__bg--buy-pion">
+      <div className="page page--buy-pion page--centered flex flex-col md:px-10 gap-6">
         <p className="text-2xl font-medium mr-auto">Buy $PION</p>
         <p className="text-xl mr-auto">[Where to buy explainer]</p>
-        <div className="steps flex flex-col gap-12 md:gap-8 md:flex-row justify-between">
-          {shop.map((item) => (
-            <ShopCard key={item.id} item={item} />
-          ))}
+        <div className="steps flex w-full flex-col gap-12 md:gap-8 md:flex-row justify-between">
+          <div
+            className={`shop-card flex flex-col items-center py-10 px-11 justify-between bg-cover bg-no-repeat bg-center rounded-3xl h-[270px] md:w-1/3 gap-4`}
+            style={{
+              backgroundImage: `url(/assets/images/buy-pion/background-1.svg)`,
+            }}
+          >
+            <span className="flex flex-col gap-2 items-center justify-center">
+              <span className="flex items-end gap-2">
+                <img src="/assets/images/buy-pion/uniswap.svg" alt="" />
+                <p className="text-2xl font-medium text-center font-tomorrow">
+                  Uniswap
+                </p>
+              </span>
+              <span className="flex gap-2">
+                <p className="text-lg font-medium text-center text-light-text">
+                  on
+                </p>
+                <img src="/assets/images/buy-pion/eth.svg" alt="" />
+                <p className="text-lg font-bold text-white text-center">
+                  Ethereum Mainnet
+                </p>
+              </span>
+            </span>
+            <button className="btn !px-5 !w-full">Buy on UNISWAP</button>
+          </div>
+
+          <div
+            className={`shop-card flex flex-col items-center py-10 px-11 justify-between bg-cover bg-no-repeat bg-center rounded-3xl h-[270px] md:w-1/3 gap-4`}
+            style={{
+              backgroundImage: `url(/assets/images/buy-pion/background-2.svg)`,
+            }}
+          >
+            <span className="flex flex-col gap-2 items-center justify-center">
+              <span className="flex items-end gap-2">
+                <img src="/assets/images/buy-pion/uniswap.svg" alt="" />
+                <p className="text-2xl font-medium text-center font-tomorrow">
+                  Uniswap
+                </p>
+              </span>
+              <span className="flex gap-2">
+                <p className="text-lg font-medium text-center text-light-text">
+                  on
+                </p>
+                <img src="/assets/images/buy-pion/arb.svg" alt="" />
+                <p className="text-lg font-bold text-white text-center">
+                  Arbitrum One
+                </p>
+              </span>
+            </span>
+            <button className="btn !px-5 !w-full">Buy on UNISWAP</button>
+          </div>
+
+          <div
+            className={`shop-card flex flex-col items-center py-10 px-11 justify-between bg-cover bg-no-repeat bg-center rounded-3xl h-[270px] md:w-1/3 gap-4`}
+            style={{
+              backgroundImage: `url(/assets/images/buy-pion/background-3.svg)`,
+            }}
+          >
+            <span className="flex flex-col gap-2 items-center justify-center">
+              <span className="flex items-end gap-2">
+                <img src="/assets/images/buy-pion/MEXC.svg" alt="" />
+                <p className="text-2xl font-medium text-center font-tomorrow">
+                  MEXC
+                </p>
+              </span>
+              <span className="flex gap-2">
+                <p className="text-lg font-medium text-center text-white">
+                  Centralized Exchange
+                </p>
+              </span>
+            </span>
+            <button className="btn !px-5 !w-full">Buy on MEXC</button>
+          </div>
         </div>
       </div>
     </div>
   );
 };
 
-const ShopCard = ({ item }: { item: Shop }) => {
-  return (
-    <div
-      className="shop-card flex flex-col items-center justify-center bg-cover bg-no-repeat bg-center gap-4 w-full md:w-1/3"
-      style={{ background: `${item.background}` }}
-    >
-      <img src={item.img} alt="" className="w-16 h-16" />
-      <p className="text-lg font-medium text-center">{item.title}</p>
-      <p className="text-lg font-medium text-center">{item.network}</p>
-      <p className="text-lg font-medium text-center">{item.description}</p>
-    </div>
-  );
-};
 export default Home;
