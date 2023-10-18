47d79638fbb812de853c9f700efd032f8fa5561d
Ali Beigi <beigi.work@gmail.com>
2023-10-19 01:17:52 +0400

    Update sidenav transition

--- src/components/Basic/Sidebar.tsx
+++ src/components/Basic/Sidebar.tsx
@@ -11,12 +11,12 @@ export const Sidebar = () => {
 
   return (
     <div
-      className={`sidebar flex flex-col z-[1000] bg-so-dark-gray p-[18px] pt-6 w-[110px] transition-all absolute left-0 top-0 h-full ${
+      className={`sidebar flex flex-col z-[1000] bg-so-dark-gray p-[18px] pt-6 w-[110px] transition-all ease-out duration-500 absolute left-0 top-0 h-full ${
         isSidebarOpen ? '!w-[213px]' : ''
       }`}
     >
       <div
-        className={`sidebar__logo flex transition-all items-center gap-4 pl-[22px] mb-10 ${
+        className={`sidebar__logo flex transition-all ease-out duration-500 items-center gap-4 pl-[22px] mb-10 ${
           isSidebarOpen && '!pl-0'
         }`}
         onClick={() => navigate('/pion/getting-started')}
@@ -45,7 +45,7 @@ export const Sidebar = () => {
       />
 
       <p
-        className={`text-light-text text-sm pl-3.5 mb-[18px] transition-all ${
+        className={`text-light-text text-sm pl-3.5 mb-[18px] transition-all ease-out duration-500 ${
           isSidebarOpen && '!pl-0 !mb-3'
         }`}
       >
@@ -105,10 +105,10 @@ export const Sidebar = () => {
 
       {isSidebarOpen && (
         <section className="links flex flex-col gap-3 w-full items-center">
-          <p className="cursor-pointer underline text-light-text font-medium hover:text-white transition-all">
+          <p className="cursor-pointer underline text-light-text font-medium hover:text-white transition-all ease-out duration-500">
             Muon Explorer
           </p>
-          <p className="cursor-pointer underline text-light-text font-medium hover:text-white transition-all">
+          <p className="cursor-pointer underline text-light-text font-medium hover:text-white transition-all ease-out duration-500">
             Build on Muon
           </p>
         </section>
@@ -170,7 +170,7 @@ export const Sidebar = () => {
       )}
       <img
         onClick={() => setIsSidebarOpen(!isSidebarOpen)}
-        className={`absolute z-[1001] transition-all left-[110px] cursor-pointer -translate-x-1/2 top-7 ${
+        className={`absolute z-[1001] transition-all ease-out duration-500 left-[110px] cursor-pointer -translate-x-1/2 top-7 ${
           isSidebarOpen && 'rotate-180 left-[213px]'
         }`}
         src="/assets/images/sidebar/arrow.svg"
@@ -197,24 +197,24 @@ const SidebarItem = ({
 }) => {
   return (
     <div
-      className={`sidebar__logo relative flex transition-all cursor-pointer items-center group hover:text-primary-L1 gap-0 pl-1.5 mb-10 ${
+      className={`sidebar__logo relative flex transition-all ease-out duration-500 cursor-pointer items-center group hover:text-primary-L1 gap-0 pl-1.5 mb-10 ${
         isSidebarOpen && '!pl-0 gap-4'
       } ${isActive && 'text-primary-L1'} ${className}`}
       onClick={onClick}
     >
       <div
-        className={`sidebar__item flex justify-center transition-all items-center group-hover:bg-primary-L1 bg-body-background gap-3 rounded-lg w-[60px] h-[60px] ${
+        className={`sidebar__item flex justify-center transition-all ease-out duration-500 items-center group-hover:bg-primary-L1 bg-body-background gap-3 rounded-lg w-[60px] h-[60px] ${
           isSidebarOpen && '!w-[42px] !h-[42px]'
         } ${isActive && 'bg-primary-L1'}`}
       >
         <img
           src={icon}
-          className={`transition-all h-[28px] ${isSidebarOpen && '!h-[24px]'}`}
+          className={`transition-all ease-out duration-500 h-[28px] ${isSidebarOpen && '!h-[24px]'}`}
           alt=""
         />
       </div>
       <p
-        className={`group-hover:text-primary-L1 text-white transition-all text-[14px] font-medium line-clamp-1 w-0 ${
+        className={`group-hover:text-primary-L1 text-white transition-all ease-out duration-500 text-[14px] font-medium line-clamp-1 w-0 ${
           isSidebarOpen && '!w-[110px]'
         } ${isActive && '!text-primary-L1'}`}
       >
@@ -222,9 +222,9 @@ const SidebarItem = ({
       </p>
 
       {!isSidebarOpen && (
-        <span className="absolute bg-light-text left-20 w-0 rounded transition-all group-hover:w-max group-hover:p-3">
+        <span className="absolute bg-light-text left-20 w-0 rounded transition-all ease-out duration-500 group-hover:w-max group-hover:p-3">
           <p
-            className={`text-black transition-all text-sm font-medium line-clamp-1 w-full`}
+            className={`text-black transition-all ease-out duration-200 text-sm font-medium line-clamp-1 w-full`}
           >
             {title}
           </p>
