b7e67740cadff71620e0e94006345b06e508a620
Amirerfan Siamaki <amirerfan.siamaki@gmail.com>
2023-10-18 12:28:21 +0200

    Change Animation

--- src/pages/Home/StepCard.tsx
+++ src/pages/Home/StepCard.tsx
@@ -1,5 +1,5 @@
 import { Step } from '../../types';
-import { MoveUpIn } from '../../animations';
+import { FadeIn } from '../../animations';
 
 export const StepCard = ({
   step,
@@ -13,7 +13,7 @@ export const StepCard = ({
   children: React.ReactNode;
 }) => {
   return (
-    <MoveUpIn className={className + ' rounded-xl'} delay={animationDelay}>
+    <FadeIn className={className + ' rounded-xl'} delay={animationDelay}>
       <div className="step-card px-7 pt-5 pb-10 h-full min-h-[297px] max-w-[430px] flex flex-col relative bg-black1 bg-opacity-50 rounded-xl backdrop-blur-[12.5px]">
         <p className="font-tomorrow absolute right-7 -top-10 text-8xl font-medium with-stroke">
           {step.id}
@@ -21,6 +21,6 @@ export const StepCard = ({
 
         {children}
       </div>
-    </MoveUpIn>
+    </FadeIn>
   );
 };
