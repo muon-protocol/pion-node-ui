06559b6e37d96ec1f7af2a3b8b60363dab9cf19f
Amirerfan Siamaki <amirerfan.siamaki@gmail.com>
2023-10-22 20:16:41 +0200

    Fix Sidebar Scroll

--- src/components/Basic/Sidebar.tsx
+++ src/components/Basic/Sidebar.tsx
@@ -23,131 +23,138 @@ export const Sidebar = () => {
 
   return (
     <div
-      className={`sidebar flex flex-col z-[1000] bg-so-dark-gray p-[18px] pt-6 w-[110px] transition-all ease-in sticky duration-[0.2s] left-0 top-0 h-[100vh] ${
+      className={`sidebar z-[1000] bg-[#18171a] transition-all ease-in sticky duration-[0.2s] left-0 top-0 h-[100vh] ${
         isSidebarOpen ? '!w-[213px]' : ''
       }`}
     >
-      <div
-        className={`sidebar__logo flex transition-all ease-in duration-[0.2s] items-center gap-4 pl-[22px] mb-10 ${
-          isSidebarOpen && '!pl-0'
-        }`}
-        onClick={() => navigate('/pion/getting-started')}
-      >
-        <img
-          src="/assets/images/sidebar/logo.svg"
-          className="w-[26px] h-8"
-          alt=""
-        />
-        {isSidebarOpen && (
-          <img src="/assets/images/sidebar/logo-typo.svg" className="" alt="" />
-        )}
-      </div>
-
-      <SidebarItem
-        className="mb-[18px]"
-        title="Get Started"
-        isSidebarOpen={isSidebarOpen}
-        isActive={location.pathname === '/pion/getting-started'}
-        onClick={() => navigate('/pion/getting-started')}
-        icon="/assets/images/sidebar/get-started.svg"
-      />
-
-      <p
-        className={`text-light-text text-sm pl-3.5 mb-[18px] transition-all ease-in duration-[0.2s] ${
-          isSidebarOpen && '!pl-0 !mb-3'
+      <span
+        className={`h-full flex flex-col py-[18px] pl-[18px] pt-6 w-[110px] transition-all ease-in duration-[0.2s] overflow-y-auto overflow-x-hidden no-scrollbar ${
+          isSidebarOpen ? '!w-[213px]' : ''
         }`}
       >
-        Steps:
-      </p>
-
-      <SidebarItem
-        className="mb-[18px]"
-        title="Buy PION"
-        isSidebarOpen={isSidebarOpen}
-        isActive={location.pathname === '/pion/buy-pion'}
-        onClick={() => navigate('/pion/buy-pion')}
-        icon="/assets/images/sidebar/step-1.svg"
-      />
-
-      <SidebarItem
-        className="mb-[18px]"
-        title={
-          bonALICEs.length + nodeBonALICE.length > 0
-            ? 'Manage bonPION'
-            : 'Create bonPION'
-        }
-        isSidebarOpen={isSidebarOpen}
-        isActive={[
-          ActionType.VIEW,
-          ActionType.CREATE,
-          ActionType.UPGRADE,
-          ActionType.MERGE,
-          ActionType.TRANSFER,
-          ActionType.SPLIT,
-        ].includes(location.pathname as ActionType)}
-        onClick={() => {
-          if (bonALICEs.length + nodeBonALICE.length > 0) {
-            navigate('/pion/bonPION/increase');
-          } else {
-            navigate('/pion/bonPION/create');
-          }
-        }}
-        icon="/assets/images/sidebar/step-2.svg"
-      />
-
-      <SidebarItem
-        className="mb-10"
-        title="Finalize Setup"
-        isSidebarOpen={isSidebarOpen}
-        isActive={location.pathname === '/pion/setup-node'}
-        onClick={() => navigate('/pion/setup-node')}
-        icon="/assets/images/sidebar/step-3.svg"
-      />
-
-      <section className="stats w-full flex gap-3 mb-7 mt-auto">
-        <div className="stats__left flex flex-col gap-3">
-          <StatItem value={stats?.annual_percentage_yield} title="Nope APR" />
-          <StatItem value={stats?.pion_staked_in_staking} title="Staked" />
-          <StatItem value={stats?.total_value_locked} title="TVL" />
+        <div
+          className={`sidebar__logo flex transition-all ease-in duration-[0.2s] items-center gap-4 pl-[22px] mb-10 ${
+            isSidebarOpen && '!pl-0'
+          }`}
+          onClick={() => navigate('/pion/getting-started')}
+        >
+          <img
+            src="/assets/images/sidebar/logo.svg"
+            className="w-[26px] h-8"
+            alt=""
+          />
+          {isSidebarOpen && (
+            <img
+              src="/assets/images/sidebar/logo-typo.svg"
+              className=""
+              alt=""
+            />
+          )}
         </div>
-        {isSidebarOpen && (
-          <FadeIn duration={0.3} delay={0.1} className="w-full">
-            <div className="stats__left flex flex-col gap-3">
-              <StatItem value={stats?.protocol_owned_liquidity} title="POL" />
-              <StatItem value={stats?.market_cap} title="MCAP" />
-              <StatItem value={stats?.pion_in_circulation} title="Supply" />
-            </div>
-          </FadeIn>
-        )}
-      </section>
 
-      {isSidebarOpen && (
+        <SidebarItem
+          className="mb-[18px]"
+          title="Get Started"
+          isSidebarOpen={isSidebarOpen}
+          isActive={location.pathname === '/pion/getting-started'}
+          onClick={() => navigate('/pion/getting-started')}
+          icon="/assets/images/sidebar/get-started.svg"
+        />
+
+        <p
+          className={`text-light-text text-sm pl-3.5 mb-[18px] transition-all ease-in duration-[0.2s] ${
+            isSidebarOpen && '!pl-0 !mb-3'
+          }`}
+        >
+          Steps:
+        </p>
+
+        <SidebarItem
+          className="mb-[18px]"
+          title="Buy PION"
+          isSidebarOpen={isSidebarOpen}
+          isActive={location.pathname === '/pion/buy-pion'}
+          onClick={() => navigate('/pion/buy-pion')}
+          icon="/assets/images/sidebar/step-1.svg"
+        />
+
+        <SidebarItem
+          className="mb-[18px]"
+          title={
+            bonALICEs.length + nodeBonALICE.length > 0
+              ? 'Manage bonPION'
+              : 'Create bonPION'
+          }
+          isSidebarOpen={isSidebarOpen}
+          isActive={[
+            ActionType.VIEW,
+            ActionType.CREATE,
+            ActionType.UPGRADE,
+            ActionType.MERGE,
+            ActionType.TRANSFER,
+            ActionType.SPLIT,
+          ].includes(location.pathname as ActionType)}
+          onClick={() => {
+            if (bonALICEs.length + nodeBonALICE.length > 0) {
+              navigate('/pion/bonPION/increase');
+            } else {
+              navigate('/pion/bonPION/create');
+            }
+          }}
+          icon="/assets/images/sidebar/step-2.svg"
+        />
+
+        <SidebarItem
+          className="mb-10"
+          title="Finalize Setup"
+          isSidebarOpen={isSidebarOpen}
+          isActive={location.pathname === '/pion/setup-node'}
+          onClick={() => navigate('/pion/setup-node')}
+          icon="/assets/images/sidebar/step-3.svg"
+        />
+
+        <section className="stats w-full flex gap-6 mb-7 mt-auto">
+          <div className="stats__left flex flex-col gap-3">
+            <StatItem value={stats?.annual_percentage_yield} title="Nope APR" />
+            <StatItem value={stats?.pion_staked_in_staking} title="Staked" />
+            <StatItem value={stats?.total_value_locked} title="TVL" />
+          </div>
+          {isSidebarOpen && (
+            <FadeIn duration={0.3} delay={0.1}>
+              <div className="stats__left flex flex-col gap-3">
+                <StatItem value={stats?.protocol_owned_liquidity} title="POL" />
+                <StatItem value={stats?.market_cap} title="MCAP" />
+                <StatItem value={stats?.pion_in_circulation} title="Supply" />
+              </div>
+            </FadeIn>
+          )}
+        </section>
+
         <section className="links flex flex-col gap-3 w-full items-center">
           <p
-            className={`cursor-pointer text-center underline text-light-text font-medium hover:text-white transition-all ease-in duration-[0.1s] line-clamp-1 w-0 ${
-              isSidebarOpen && '!w-[300px]'
+            className={`cursor-pointer text-center h-0 underline text-light-text font-medium hover:text-white transition-all ease-in duration-[0.1s] line-clamp-1 opacity-0 ${
+              isSidebarOpen && 'opacity-100 !h-5'
             }`}
           >
             Muon Explorer
           </p>
           <p
-            className={`cursor-pointer text-center underline text-light-text font-medium hover:text-white transition-all ease-in duration-[0.1s] line-clamp-1 w-0 ${
-              isSidebarOpen && '!w-[300px]'
+            className={`cursor-pointer text-center h-0 underline text-light-text font-medium hover:text-white transition-all ease-in duration-[0.1s] line-clamp-1 opacity-0 ${
+              isSidebarOpen && 'opacity-100 !h-5'
             }`}
           >
             Build on Muon
           </p>
         </section>
-      )}
-
-      <img
-        onClick={() => setIsSidebarOpen(!isSidebarOpen)}
-        className={`absolute z-[1001] transition-all ease-in duration-[0.2s] left-[110px] cursor-pointer -translate-x-1/2 top-7 ${
-          isSidebarOpen && 'rotate-180 left-[213px]'
-        }`}
-        src="/assets/images/sidebar/arrow.svg"
-        alt=""
-      />
+        <img
+          onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+          className={`absolute z-[1001] transition-all ease-in duration-[0.2s] left-[110px] cursor-pointer -translate-x-1/2 top-7 ${
+            isSidebarOpen && 'rotate-180 left-[213px]'
+          }`}
+          src="/assets/images/sidebar/arrow.svg"
+          alt=""
+        />
+      </span>
     </div>
   );
 };
@@ -188,15 +195,15 @@ const SidebarItem = ({
         />
       </div>
       <p
-        className={`group-hover:text-primary-L1 text-white transition-all ease-in duration-[0.2s] text-[14px] font-medium line-clamp-1 w-0 ${
-          isSidebarOpen && '!w-[125px]'
+        className={`group-hover:text-primary-L1 text-white transition-all ease-in duration-[0.2s] text-[14px] font-medium line-clamp-1 w-0 opacity-0 ${
+          isSidebarOpen && '!w-[125px] opacity-100'
         } ${isActive && '!text-primary-L1 !font-bold'}`}
       >
         {title}
       </p>
 
       {!isSidebarOpen && (
-        <span className="absolute bg-light-text left-20 w-0 rounded transition-all ease-in duration-[0.2s] group-hover:w-max group-hover:p-3">
+        <span className="fixed bg-light-text left-[92px] w-0 rounded transition-all ease-in duration-[0.2s] opacity-0 group-hover:w-max group-hover:p-3 group-hover:opacity-100">
           <p
             className={`text-black transition-all ease-out duration-300 text-sm font-medium line-clamp-1 w-full ${
               isActive && 'text-primary !font-bold'
