2497ed8264f63c077db77cfc3569ee893d294a36
Amirerfan Siamaki <amirerfan.siamaki@gmail.com>
2023-10-31 10:54:13 +0100

    Migration Page New Changes

--- src/pages/Migration/index.tsx
+++ src/pages/Migration/index.tsx
@@ -61,18 +61,34 @@ const ConnectWalletBody = () => {
 };
 
 const ClaimTokenBody = () => {
+  if (Math.random() > 0.5) {
+    return <ClaimTokenWalletWithBalance />;
+  }
+  return <ClaimTokenWalletWithoutBalance />;
+};
+
+const ClaimTokenWalletWithBalance = () => {
   return (
     <>
       <p className="text-center text-xl font-medium max-w-[517px]">
         [Explainer: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
         do eiusmod tempor incididunt ut labore et dolore magna ]
       </p>
-      <div className="card bg-primary-L1-50 rounded-[18px] pt-7 pl-11 pr-10 pb-8 max-w-[517px]">
-        <p className="text-white mb-10 text-lg font-medium">
-          You had <strong>4,000 PION</strong> tokens in your wallet at snapshot
-          taken on October 23, 2023, at 14:55:42 (Ethereum Mainnet Block
-          #1234567)
-        </p>
+      <div className="card bg-primary-L1-50 rounded-[18px] flex flex-col justify-between min-h-[290px] pt-7 pl-14 pr-10 pb-8 w-[517px]">
+        <ul className="list-disc">
+          {Math.random() > 0.5 && (
+            <li className="text-white mb-3 text-lg font-medium">
+              You had <strong>4,000 PION</strong> tokens in your wallet at
+              snapshot taken on October 23, 2023, at 14:55:42 (Ethereum Mainnet
+              Block #1234567).
+            </li>
+          )}
+          {Math.random() > 0.5 && (
+            <li className="text-white mb-10 text-lg font-medium">
+              You had <strong>4,000 PION</strong> tokens in MEXC exchange.
+            </li>
+          )}
+        </ul>
         <button className="btn btn--white btn--medium-with-icon mx-auto">
           <img
             className="h-6 w-6"
@@ -81,6 +97,23 @@ const ClaimTokenBody = () => {
           />
           <p className="text-inherit">Claim PION</p>
         </button>
+      </div>
+    </>
+  );
+};
+
+const ClaimTokenWalletWithoutBalance = () => {
+  return (
+    <>
+      <p className="text-center text-xl font-medium max-w-[517px]">
+        [Explainer: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+        do eiusmod tempor incididunt ut labore et dolore magna ]
+      </p>
+      <div className="card bg-card-bg-v2 rounded-[18px] flex flex-col gap-6 justify-center items-center p-12 pb-8">
+        <img src="/assets/images/migration/no-record-icon.svg" alt="" />
+        <p className="text-gary4 text-lg font-medium">
+          No record found, try another address
+        </p>
       </div>
     </>
   );
