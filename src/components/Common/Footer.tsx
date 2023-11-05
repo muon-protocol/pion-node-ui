c6b1c2abc5787c63d8586254def961120f1a1562
Amirerfan Siamaki <amirerfan.siamaki@gmail.com>
2023-11-05 14:05:47 +0100

    Fix Footer Bug

--- src/components/Common/Footer.tsx
+++ src/components/Common/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <footer className="footer p-6 flex items-center justify-center absolute bottom-0 w-full">
+    <footer className="footer p-6 flex items-center justify-center absolute -translate-y-full w-full">
       <img src="/assets/images/footer/logo.svg" alt="" />
     </footer>
   );
