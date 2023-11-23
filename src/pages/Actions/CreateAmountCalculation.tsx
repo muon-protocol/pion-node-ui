7fe2ea7aeaabf207641ab49e73d3cae824b1335e
Amirerfan Siamaki <amirerfan.siamaki@gmail.com>
2023-11-23 21:38:49 +0100

    Fix Bug

--- src/pages/Actions/CreateAmountCalculation.tsx
+++ src/pages/Actions/CreateAmountCalculation.tsx
@@ -1,12 +1,10 @@
 import { MoveUpIn } from '../../animations';
 import useCreateAction from '../../contexts/CreateAction/useCreateAction.ts';
-import { useTokenPrice } from '../../hooks/tokenPrice/useTokenPrice.ts';
 import { useBooster } from '../../hooks/booster/useBooster.ts';
 import strings from '../../constants/strings.ts';
 
 const CreateAmountCalculation = () => {
   const { createAmount, createBoostAmount } = useCreateAction();
-  const { ALICEPrice } = useTokenPrice();
   const { boostCoefficient } = useBooster();
 
   if (!createAmount.hStr && !createBoostAmount.hStr) return null;
@@ -14,28 +12,26 @@ const CreateAmountCalculation = () => {
     return null;
   if (!boostCoefficient) return null;
 
-  if (!ALICEPrice) {
-    return (
-      <MoveUpIn
-        y={-10}
-        duration={0.1}
-        delay={0.1}
-        className="flex mt-4 w-full justify-between items-center"
-      >
-        <span className="text-gray10">
-          <p className="font-light">New {strings.nft} amount:</p>
-          <p className="h-5"></p>
-        </span>
-        <span className="rounded-md bg-primary-dark dark:bg-alice-primary px-3 py-2.5 text-xl font-bold text-white">
-          {(
-            Number(Number(createAmount.hStr).toFixed(2)) * boostCoefficient?.dsp
-          ).toFixed(2) +
-            ' ' +
-            strings.token}
-        </span>
-      </MoveUpIn>
-    );
-  }
+  return (
+    <MoveUpIn
+      y={-10}
+      duration={0.1}
+      delay={0.1}
+      className="flex mt-4 w-full justify-between items-center"
+    >
+      <span className="text-gray10">
+        <p className="font-light">New {strings.nft} amount:</p>
+        <p className="h-5"></p>
+      </span>
+      <span className="rounded-md bg-primary-dark dark:bg-alice-primary px-3 py-2.5 text-xl font-bold text-white">
+        {(
+          Number(Number(createAmount.hStr).toFixed(2)) * boostCoefficient?.dsp
+        ).toFixed(2) +
+          ' ' +
+          strings.token}
+      </span>
+    </MoveUpIn>
+  );
 };
 
 export default CreateAmountCalculation;
