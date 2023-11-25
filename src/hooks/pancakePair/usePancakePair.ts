2ca0900e3aa7fdfd3333a7be7639e5ad439696e4
Amirerfan Siamaki <amirerfan.siamaki@gmail.com>
2023-11-25 18:07:58 +0100

    Fix Build Bugs

--- src/hooks/pancakePair/usePancakePair.ts
+++ src/hooks/pancakePair/usePancakePair.ts
@@ -1,34 +1,34 @@
-import { usePancakePairGetReserves } from '../../abis/types/generated.ts';
-import { PANCAKE_PAIR_ADDRESS } from '../../constants/addresses.ts';
-import { getCurrentChainId } from '../../constants/chains.ts';
-import { useMemo } from 'react';
-import { w3bNumberFromBigint } from '../../utils/web3.ts';
-
-export const usePancakePair = () => {
-  const { data: reservesData } = usePancakePairGetReserves({
-    address: PANCAKE_PAIR_ADDRESS[getCurrentChainId()],
-  });
-
-  const ALICEPrice = useMemo(() => {
-    if (reservesData) {
-      return (
-        w3bNumberFromBigint(reservesData[0]).dsp /
-        w3bNumberFromBigint(reservesData[1]).dsp
-      );
-    }
-  }, [reservesData]);
-
-  const USDCPrice = useMemo(() => {
-    if (reservesData) {
-      return (
-        w3bNumberFromBigint(reservesData[1]).dsp /
-        w3bNumberFromBigint(reservesData[0]).dsp
-      );
-    }
-  }, [reservesData]);
-
-  return {
-    ALICEPrice: ALICEPrice,
-    USDCPrice: USDCPrice,
-  };
-};
+// import { usePancakePairGetReserves } from '../../abis/types/generated.ts';
+// import { PANCAKE_PAIR_ADDRESS } from '../../constants/addresses.ts';
+// import { getCurrentChainId } from '../../constants/chains.ts';
+// import { useMemo } from 'react';
+// import { w3bNumberFromBigint } from '../../utils/web3.ts';
+//
+// export const usePancakePair = () => {
+//   const { data: reservesData } = usePancakePairGetReserves({
+//     address: PANCAKE_PAIR_ADDRESS[getCurrentChainId()],
+//   });
+//
+//   const ALICEPrice = useMemo(() => {
+//     if (reservesData) {
+//       return (
+//         w3bNumberFromBigint(reservesData[0]).dsp /
+//         w3bNumberFromBigint(reservesData[1]).dsp
+//       );
+//     }
+//   }, [reservesData]);
+//
+//   const USDCPrice = useMemo(() => {
+//     if (reservesData) {
+//       return (
+//         w3bNumberFromBigint(reservesData[1]).dsp /
+//         w3bNumberFromBigint(reservesData[0]).dsp
+//       );
+//     }
+//   }, [reservesData]);
+//
+//   return {
+//     ALICEPrice: ALICEPrice,
+//     USDCPrice: USDCPrice,
+//   };
+// };
